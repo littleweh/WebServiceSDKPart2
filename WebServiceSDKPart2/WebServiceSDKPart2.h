8a197784446c46616d84236108a64e42a44d0868
Ada Kao <adakao@zdev.local>
2018-04-26 18:21:57 +0800

    first commit

---
+++ WebServiceSDKPart2/WebServiceSDKPart2.h
@@ -0,0 +1,13 @@
+//
+//  WebServiceSDKUsingDelegate.h
+//  WebServiceSDKPart2
+//
+//  Created by Ada Kao on 26/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebServiceSDKPart2 : NSObject
+
+@end
---
+++ WebServiceSDKPart2/WebServiceSDKPart2.m
@@ -0,0 +1,13 @@
+//
+//  WebServiceSDKUsingDelegate.m
+//  WebServiceSDKPart2
+//
+//  Created by Ada Kao on 26/04/2018.
+//  Copyright © 2018 Ada Kao. All rights reserved.
+//
+
+#import "WebServiceSDKPart2.h"
+
+@implementation WebServiceSDKPart2
+
+@end
